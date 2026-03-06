c2a73ee6d5c5b7cab5885efd834b70bcbc364824
25LP2040 25LP2040 <michibaysaa@gmail.com>
2026-03-06 10:41:02 +0800

    3

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_WEBHOOK_KEY: process.env.CLERK_WEBHOOK_KEY,
   },
 };
 
